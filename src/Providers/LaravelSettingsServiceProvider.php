9af46ae6c0eecbb9d998f25710e75577a7de27dd
GIGABAIT93 <59637348+GIGABAIT93@users.noreply.github.com>
2025-08-15 20:50:07 +0300

    fix

--- src/Providers/LaravelSettingsServiceProvider.php
+++ src/Providers/LaravelSettingsServiceProvider.php
@@ -2,18 +2,42 @@
 
 namespace Gigabait93\LaravelSettings\Providers;
 
+use Illuminate\Support\ServiceProvider;
 use Gigabait93\LaravelSettings\Services\SettingsService;
 
-class LaravelSettingsServiceProvider
+class LaravelSettingsServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        $this->app->singleton(SettingsService::class, function ($app) {
-            return new SettingsService();
-        });
+        $this->app->singleton(SettingsService::class, fn ($app) => new SettingsService());
+        $this->mergeConfigFrom(__DIR__ . '/../../config/settings.php', 'settings');
     }
 
     public function boot(): void
     {
+        $this->loadMigrationsFrom(__DIR__ . '/../../database/migrations');
+
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__ . '/../../config/settings.php' => config_path('settings.php'),
+            ], 'config');
+
+            if (! $this->migrationPublished('create_settings_table.php')) {
+                $timestamp = date('Y_m_d_His');
+                $this->publishes([
+                    __DIR__ . '/../../database/migrations/create_settings_table.php.stub'
+                    => database_path("migrations/{$timestamp}_create_settings_table.php"),
+                ], 'migrations');
+            }
+
+            $this->publishes([
+                __DIR__ . '/../../database/migrations/' => database_path('migrations'),
+            ], 'laravel-settings-migrations');
+        }
+    }
+
+    protected function migrationPublished(string $file): bool
+    {
+        return !empty(glob(database_path("migrations/*_{$file}")));
     }
 }
